ec51ee39f11665e6e54881136491534e435c319b
Enialz.damvel@hotmail.fr <Enialz.damvel@hotmail.fr>
2018-06-27 16:10:47 +0200

    premier combat

--- manager/RpgKing.java
+++ manager/RpgKing.java
@@ -18,21 +18,23 @@ import com.imie.algotojava.tp09Modifie.model.*;
 public class RpgKing {
 	
 	
-	public static void commencer() {
+	public static  void commencer() {
 		int nbrJoueurs = 0;
 		ArrayList<Hero> Joueurs = new ArrayList<Hero>();
-		Barbare unBarbare = new Barbare();
+		ArmePhysique armeBarbare = new ArmePhysique("CartonJaune",1,5,5);
+		ArmurePhysique armureBarbare = new ArmurePhysique("GiletJaune",1,2);
+		Barbare unBarbare = new Barbare(armeBarbare,armureBarbare);
 		Paladin unPaladin = new Paladin();
 		Magicien unMagicien = new Magicien();
 		System.out.println("Combien de joueurs?");
 		Scanner sc = new Scanner(System.in);
 		nbrJoueurs = sc.nextInt();
 		Joueurs = createPlayer(nbrJoueurs, unBarbare, unPaladin, unMagicien);
-		game(Joueurs);
+		game(Joueurs, unBarbare, unPaladin, unMagicien);
 
 	}
 	
-	public static ArrayList<Hero> createPlayer(int nbrJoueurs,Barbare unBarbare,Paladin unPaladin,Magicien unMagicien)
+	public static  ArrayList<Hero> createPlayer(int nbrJoueurs,Barbare unBarbare,Paladin unPaladin,Magicien unMagicien)
 	{
 		String saisieUser = "";
 		Scanner sc = new Scanner(System.in);
@@ -61,11 +63,9 @@ public class RpgKing {
 					case 2:
 						
 						do{
-							saisieUser = "";
 							System.out.println("Classe de "+tabPerso[0]+" "+tabPerso[1]);
 							System.out.println("Barbare = 1\nPaladin = 2\nMagicien = 3");
 							saisieUser = sc.nextLine();
-							System.out.println("a:" + saisieUser + ":a");
 							if(saisieUser.equals("1") && saisieUser.equals("2") && saisieUser.equals("3"))
 							{
 								System.out.println("Erreur de saisie" );
@@ -141,23 +141,78 @@ public class RpgKing {
 		return Joueurs;
 	}
 	
-	public static void game(ArrayList<Hero> joueurs) {
+	public static  void game(ArrayList<Hero> joueurs,Barbare unBarbare,Paladin unPaladin,Magicien unMagicien) {
 		
 		//variable
-		ArrayList<Hero> Joueurs =  joueurs;
 		String saisieUser = "";
 		Scanner sc = new Scanner(System.in);
 		String tabPerso[] = new String[7];
 		String classe = "";
+		String winner="";
 		
-		//debut du jeu
-		for(int i =0; i < Joueurs.size();i++) {
+		//Affichage Personnages 
+		for(int i =0; i < joueurs.size();i++) {
 			int a = i+1;
-			System.out.println("Nom du joueur"+a+": "+Joueurs.get(i).getPrenom());
-			System.out.println("Nom du joueur"+a+": "+Joueurs.get(i).getNom());
-			
+			System.out.println("Nom du joueur"+a+": "+joueurs.get(i).getPrenom());
+			System.out.println("Nom du joueur"+a+": "+joueurs.get(i).getNom());
+			System.out.println("Arme du joueur"+a+": "+joueurs.get(i).getArme().getNom());
+			System.out.println("Armure du joueur"+a+": "+joueurs.get(i).getArmure().getNom());
 		}
+		
+		Mob gobelin = apparitionGobelinBarbare(unBarbare);
+		winner = combat(joueurs,gobelin);
+		System.out.println("le gagnant hero vs gobelin est "+winner);
+		
 	}
 		
+	public static  Mob apparitionGobelinBarbare(Barbare unBarbare)
+	{
+		Mob Gobelin = new Mob("Roi","Gobelin",1,80,5,5,unBarbare);
+		return Gobelin;
+	}
 	
+	public static  String combat(ArrayList<Hero> joueurs,Mob mob)
+	{
+		Scanner sc = new Scanner(System.in);
+		String winner ="";
+		while(mob.getVie() > 0 || joueurs.isEmpty())
+		for(int i = 0;i < joueurs.size();i++)
+		{
+			// le joueur attaque le monstre
+			System.out.println("Le "+joueurs.get(i).getNom()+"attaque a"+joueurs.get(i).getAttaque()[0]+" degats");
+			sc.nextLine();
+			mob.setVie(mob.getVie() - joueurs.get(i).getAttaque()[0]);
+			
+			//Affichage points de vie restant
+			System.out.println("Il reste "+mob.getVie()+" PDV au "+mob.getPrenom()+mob.getNom());
+			
+			//Si il  est vivant il peut attaquer
+			if(mob.getVie() < 1)
+			{
+				System.out.println(mob.getPrenom()+" "+mob.getNom()+" is dead");
+				winner = joueurs.get(i).getNom()+" "+joueurs.get(i).getPrenom();
+				joueurs.remove(joueurs.get(i));
+				break;
+			}else {
+				// Affichage Attaque mob
+				System.out.println("Le mob fait "+mob.getAttaque()[0]+" degats");
+				
+			 	joueurs.get(i).setVie(joueurs.get(i).getVie() - mob.getAttaque()[0]); 
+			 	
+			 	System.out.println("PDV Joueur"+joueurs.get(i).getNom()+" "+joueurs.get(i).getPrenom()+": " +joueurs.get(i).getVie());
+				if(joueurs.get(i).getVie() < 1)
+				{
+					System.out.println(joueurs.get(i).getNom()+" "+joueurs.get(i).getPrenom()+" is dead");
+					winner = mob.getNom()+" "+mob.getPrenom();
+					break;
+				}
+			}
+		}
+		return winner;
+	}
+	
+	public static void attaquer()
+	{
+		
+	}
 }
